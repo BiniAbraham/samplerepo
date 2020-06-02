68989e06f96937ebff7dc014aa6588bec611106f
BiniAbraham <biniabraham99@gmail.com>
2020-06-02 20:25:45 +0530

    second commit

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 var express=require('express');
 
 const server=express()
-
+server.set("view engine","ejs");
+server.set("views","./pages");
 server.listen(8000,function(){
     console.log("server started listening....")
 });
 
 
-server.get("/",function(req,res){
-       res.sendFile(__dirname+"/views/home.html")
-})
-
-
-server.get("/home",function(req,res){
-    res.send("this home page")
-})
-
 server.get("/login",function(req,res){
-    res.sendFile(__dirname+"/views/login.html")
+       res.render("login")
+})
+
+server.get("/aboutus",function(req,res){
+    res.render("aboutus")
+})
+server.get("/services",function(req,res){
+    res.render("services")
+})
+server.get("/validate",function(req,res){
+    res.render("home")
+})
+server.get("/home",function(req,res){
+    res.render("home")
 })
